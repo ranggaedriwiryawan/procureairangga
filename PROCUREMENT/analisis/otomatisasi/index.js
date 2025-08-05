61c367a8670b5a96f9947f9d57796a9c6a114f70
ranggaedriwiryawan <ranggaedriwiryawan56@gmail.com>
2025-08-05 09:21:02 +0700

    Delete PROCUREMENT directory

--- PROCUREMENT/analisis/otomatisasi/index.js
+++
@@ -1,8 +0,0 @@
-// analisis/otomatisasi/index.js
-// Modul logika khusus visualisasi otomatisasi
-
-import { automationSteps } from '../../data.js';
-
-export function getStepDescription(step) {
-  return automationSteps[step] || '';
-}
